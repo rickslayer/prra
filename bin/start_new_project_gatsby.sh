dcc05a6d27b19480ec581b5700ac188db2103fd5
rickslayer <paulo.rube@madeiramadeira.com.br>
2020-08-21 15:05:52 -0300

    gatsby configure

---
+++ bin/start_new_project_gatsby.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#to install gastsby globally npm install -g gatsby-cli
+
+echo iniciate project
+
+
+gatsby new prra-dev https://github.com/gatsbyjs/gatsby-starter-hello-world
+
+cd prra-dev
---
+++ prra-dev/gatsby-config.js
@@ -0,0 +1,70 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `PRRA Dev`,
+        short_name: `PRRADev`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#154d40`,
+        display: `standalone`,
+        icon:`src/imgs/favicon.png`
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.prra.dev`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+   
+    `gatsby-plugin-offline`,
+
+  ],
+}
